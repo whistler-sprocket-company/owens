c269abb70a0b6214b7a46f765d63f09c2ce74c86
MichaelB <mjbradvica@gmail.com>
2025-06-21 13:33:41 -0400

    conn string

--- source/Owens.API/Program.cs
+++ source/Owens.API/Program.cs
@@ -17,7 +17,7 @@ namespace Owens.API
             builder.Services.AddOpenApi();
 
             Debug.Print(builder.Configuration.GetConnectionString("Database"));
-            builder.Services.AddSqlServer<ApplicationContext>("Server=tcp:acme-sprocket-co.database.windows.net,1433;Initial Catalog=owens-dev;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=\"Active Directory Default\";");
+            builder.Services.AddSqlServer<ApplicationContext>(builder.Configuration.GetConnectionString("Database"));
 
             var app = builder.Build();
 
